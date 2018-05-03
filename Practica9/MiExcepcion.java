eed59bb8b4f61532513a9cbe5a4ee01f4c1dc0d6
AntonioAlcantara <tonialcantara1997@gmail.com>
2018-05-03 17:39:55 +0200

    Practica9

---
+++ Practica9/MiExcepcion.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practica9;
+
+
+/**
+ *
+ * @author Antonio
+ */
+public class MiExcepcion extends Exception
+{
+    public MiExcepcion (String mensaje, Throwable causa)
+    {
+        super(mensaje, causa);
+    }
+}
